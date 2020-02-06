44115d176ac797f579dfdf9d2dab15061f370d51
Giovani Aguirre <me@its-g.io>
2020-02-06 14:41:28 -0600

    Created index.js and .gitignore with .env and node_modules plus added nodemon as a package

---
+++ index.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const massive = require('massive');
+
+const app = express();
+const {SERVER_PORT} = process.env;
+
+app
+  .use(express.json())
+
+app.listen(SERVER_PORT, () => console.log(`Guess who's back?`))
