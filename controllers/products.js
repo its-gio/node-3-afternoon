a6be80f1ce0baba0c3ddd07fcfdf52932fd8d368
Giovani Aguirre <me@its-g.io>
2020-02-06 16:52:50 -0600

    Created CRUD requests and endpoints

---
+++ controllers/products.js
@@ -0,0 +1,44 @@
+module.exports.create = (req, res) => {
+  const { name, description, price, image_url } = req.body;
+  const db = req.app.get("db");
+
+  db.create_product(name, description, price, image_url)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.error(err));
+}
+
+module.exports.getOne = (req, res) => {
+  const { id } = req.params;
+  const db = req.app.get("db");
+
+  db.read_product(id)
+    .then(product => res.status(200).json(product))
+    .catch(err => console.error(err));
+}
+
+module.exports.getAll = (req, res) => {
+  const db = req.app.get("db");
+
+  db.read_products()
+    .then(productsArr => res.status(200).json(productsArr))
+    .catch(err => console.error(err))
+}
+
+module.exports.updateItem = (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  const db = req.app.get("db");
+
+  db.update_product(id, description)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.error(err));
+}
+
+module.exports.deleteItem = (req, res) => {
+  const { id } = req.params;
+  const db = req.app.get("db");
+
+  db.delete_product(id)
+    .then(() => res.status(200))
+    .catch(err => console.error(err));
+}
--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const express = require('express');
 const massive = require('massive');
 
 const app = express();
-const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+const { create, getOne, getAll, updateItem, deleteItem } = require("./controllers/products");
+
+app.use(express.json())
 
 massive(CONNECTION_STRING)
   .then(db =>  app.set("db", db))
   .catch(err => console.error(err))
 
 app
-  .use(express.json())
+  .post("/api/product", create)
+  .get("/api/product/", getAll)
+  .get("/api/product/:id", getOne)
+  .put("/api/product/:id", updateItem)
+  .delete("/api/product/:id", deleteItem)
 
 app.listen(SERVER_PORT, () => console.log(`Guess who's back?`))
